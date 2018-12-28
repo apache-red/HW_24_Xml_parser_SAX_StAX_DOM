99f78c07b1d909eaf01c972844bf11f32dd1631f
RED <apache_red@mail.ru>
2018-12-28 12:32:34 +0300

    add DOM parser 1f;

--- src/com/redcompany/red/start/dom/FamilyDomHandler.java
+++ src/com/redcompany/red/start/dom/FamilyDomHandler.java
@@ -4,10 +4,7 @@ import com.redcompany.red.entity.behavior.Hobby;
 import com.redcompany.red.entity.behavior.HobbyDayAndTime;
 import com.redcompany.red.entity.behavior.Schema;
 import com.redcompany.red.entity.person.*;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 
 public class FamilyDomHandler {
     private Element element;
@@ -21,78 +18,62 @@ public class FamilyDomHandler {
     private HobbyDayAndTime hobbyDayAndTime;
     private String textValue;
     private String tagName;
+    private String attr;
 
 
     public FamilyDomHandler() {
         society = new Society();
     }
 
-    public void parseDocument(Document document) {
 
+    public void parseDocument(Document document) {
         this.element = document.getDocumentElement();
 
-        getElement(element);
-        combineWithEntity(nodeList);
-        System.out.println();
+            this.nodeList = getNodeList(element);
+            parseNodList(this.nodeList);
 
+society.showSiciety();
     }
 
-    private void getElement(Element element) {
-        this.nodeList = element.getChildNodes();
+
+    private NodeList getNodeList(Element element) {
+        return element.getChildNodes();
     }
 
-    private void combineWithEntity(NodeList nodeList) {
+    private void parseNodList(NodeList nodeList) {
         for (int i = 0; i < nodeList.getLength(); i++) {
-            if (nodeList.item(i) instanceof Element) {
-                tagName = ((Element) nodeList.item(i)).getTagName();
-                choice();
-                family.setAdress(((Element) nodeList.item(i)).getAttribute("adress"));
-                Element current = (Element) nodeList.item(i);
-                NodeList childElements = current.getChildNodes();
-                for (int j = 0; j < childElements.getLength(); j++) {
-                    if (nodeList.item(j) instanceof Element) {
-                        tagName = ((Element) nodeList.item(j)).getTagName();
-                        choice();
-                        Element current2 = (Element) nodeList.item(j);
-                        NodeList childElements2 = current.getChildNodes();
-
-                        for (int k = 0; k < childElements2.getLength(); k++) {
-                            if (nodeList.item(k) instanceof Element) {
-                                tagName = ((Element) nodeList.item(k)).getTagName();
-                                System.out.println(tagName);
-                            }
-                        }
-
-
-                    }
-
+            if (nodeList.item(i).getNodeType() == 1) {
+                if (nodeList.item(i).getAttributes() != null) {
+                    this.attr = ((Element) nodeList.item(i)).getAttribute("adress");
                 }
-
+                tagName = nodeList.item(i).getNodeName();
+                entityMatch(tagName);
+                Text text = (Text) nodeList.item(i).getFirstChild();
+                textValue = text.getData().trim();
+                setEntityData(tagName, textValue);
+                Element current = (Element) nodeList.item(i);
+                NodeList childList = getNodeList(current);
+                parseNodList(childList);
             }
         }
+
     }
 
-
-    private void choice() {
+    private void entityMatch(String tagName) {
         switch (tagName) {
             case "family":
-                family = new Family();
-                society.addFamilyToFamilyList(family);
+                this.family = new Family();
                 break;
             case "mother":
-                familyMember = new Mother();
-                family.setMother((Mother) familyMember);
+                this.familyMember = new Mother();
                 break;
             case "father":
-                familyMember = new Father();
-                family.setFather((Father) familyMember);
+                this.familyMember = new Father();
                 break;
             case "child":
-                familyMember = new Child();
-                family.addChildToChaildList((Child) familyMember);
+                this.familyMember = new Child();
                 break;
             case "maidenname":
-
                 break;
             case "name":
                 break;
@@ -103,26 +84,77 @@ public class FamilyDomHandler {
             case "gender":
                 break;
             case "hobby":
-                hobby = new Hobby();
-                familyMember.addHobby(hobby);
+                this.hobby = new Hobby();
                 break;
             case "title":
                 break;
             case "times-per-week":
                 break;
             case "schedule":
-                schema = new Schema();
-                hobby.setSchema(schema);
+                this.schema = new Schema();
                 break;
             case "day_name":
-                hobbyDayAndTime = new HobbyDayAndTime();
-                schema.addDayAndTimeToSchemaList(hobbyDayAndTime);
+                this.hobbyDayAndTime = new HobbyDayAndTime();
                 break;
             case "time":
                 break;
             default:
                 break;
         }
-
     }
+
+    private void setEntityData(String tagName, String textValue) {
+        switch (tagName) {
+            case "family":
+                family.setAdress(attr);
+                society.addFamilyToFamilyList(family);
+                break;
+            case "mother":
+                  family.setMother((Mother) familyMember);
+                break;
+            case "father":
+                family.setFather((Father) familyMember);
+                break;
+            case "child":
+                family.addChildToChaildList((Child) familyMember);
+                break;
+            case "maidenname":
+                familyMember.setMaidenname(textValue);
+                break;
+            case "name":
+                familyMember.setName(textValue);
+                break;
+            case "surname":
+                familyMember.setSurname(textValue);
+                break;
+            case "age":
+                familyMember.setAge(Integer.parseInt(textValue));
+                break;
+            case "gender":
+                familyMember.setGender(textValue);
+                break;
+            case "hobby":
+                familyMember.addHobby(hobby);
+                break;
+            case "title":
+                hobby.setHobby_name(textValue);
+                break;
+            case "times-per-week":
+                hobby.setTimes_per_week(textValue);
+                break;
+            case "schedule":
+                hobby.setSchema(schema);
+                break;
+            case "day_name":
+                hobbyDayAndTime.setDay(textValue);
+                break;
+            case "time":
+                hobbyDayAndTime.setTime(textValue);
+                schema.addDayAndTimeToSchemaList(hobbyDayAndTime);
+                break;
+            default:
+                break;
+        }
+    }
+
 }
